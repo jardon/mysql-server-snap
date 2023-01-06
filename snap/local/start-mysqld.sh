7d252894aef2cefe184b59ed3ed46e3e87c577e4
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-06 15:21:49 -0500

    Fix cli params for mysqld

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -7,9 +7,10 @@ if [ ! -f "${CONFIG_DIR}/initialized" ]; then
         echo "copying default config to ${CONFIG_DIR}"
         cp -r $SNAP/etc/mysql $SNAP_COMMON
         sed -i "s:/var/run/mysqld/mysqld.sock:$SNAP_COMMON/mysqld.sock:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
+        sed -i "s: mysql: root:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
         sed -i "s:# sock:sock:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
         sed -i "s:/var/log/mysql/error.log:$SNAP_COMMON/error.log:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
         touch $CONFIG_DIR/initialized
 fi
 
-$SNAP/usr/sbin/mysqld --defaults-file=$SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
+$SNAP/usr/sbin/mysqld --defaults-file=$SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf --user=root
