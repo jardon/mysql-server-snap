40c88c6c18b9ca2c8d44821df0e3326f8ef562c8
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-11 15:38:47 -0500

    Add comments to wrapper script

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -5,12 +5,20 @@ if [ ! -f "${CONFIG_DIR}/initialized" ]; then
         $SNAP/usr/sbin/mysqld --initialize
         echo "configuration folder ${CONFIG_DIR} does not exist."
         echo "copying default config to ${CONFIG_DIR}"
+
+        # Copy over mysql config file
         cp -r $SNAP/etc/mysql $SNAP_COMMON
+
+        # Set file permissions so snap_daemon can write to various files/folders
         chown -R snap_daemon:root $CONFIG_DIR
 	chown -R snap_daemon:root $SNAP_DATA/var
+
+        # Replace default configuration options to work within the snap environment
         sed -i "s:/var/run/mysqld/mysqld.sock:$CONFIG_DIR/mysqld.sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
         sed -i "s:# sock:sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
         sed -i "s: mysql: snap_daemon:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
+
+        # Create file to indicate initialization has been completed
 	touch $CONFIG_DIR/initialized
 fi
 
