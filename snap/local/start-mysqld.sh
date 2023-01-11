a9560a07264285eef6214d3a98e7e48868e6afba
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-11 11:09:01 -0500

    Reconfigure file permissions and add layout mapping

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -6,13 +6,12 @@ if [ ! -f "${CONFIG_DIR}/initialized" ]; then
         echo "configuration folder ${CONFIG_DIR} does not exist."
         echo "copying default config to ${CONFIG_DIR}"
         cp -r $SNAP/etc/mysql $SNAP_COMMON
-        chmod 770 $CONFIG_DIR
         chown -R snap_daemon:root $CONFIG_DIR
+	chown -R snap_daemon:root $SNAP_DATA/var
         sed -i "s:/var/run/mysqld/mysqld.sock:$CONFIG_DIR/mysqld.sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
-        sed -i "s: mysql: snap_daemon:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
         sed -i "s:# sock:sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
-        sed -i "s:/var/log/mysql/error.log:$CONFIG_DIR/error.log:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
-        touch $CONFIG_DIR/initialized
+        sed -i "s: mysql: snap_daemon:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
+	touch $CONFIG_DIR/initialized
 fi
 
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
